f0d15849e3cf2d029c97455e9edb0a735de001f9
My Name <asdrenxhafa@gmail.com>
2020-10-08 09:55:02 +0200

    Queuing Notification and observer for Personel

---
+++ app/Observers/PersoneliObserver.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Observers;
+
+use App\Personeli;
+
+class PersoneliObserver
+{
+    /**
+     * Handle the personeli "created" event.
+     *
+     * @param  \App\Personeli  $personeli
+     * @return void
+     */
+    public function created(Personeli $personeli)
+    {
+        //
+    }
+
+    /**
+     * Handle the personeli "updated" event.
+     *
+     * @param  \App\Personeli  $personeli
+     * @return void
+     */
+    public function updated(Personeli $personeli)
+    {
+        //
+    }
+
+    /**
+     * Handle the personeli "deleted" event.
+     *
+     * @param  \App\Personeli  $personeli
+     * @return void
+     */
+    public function deleted(Personeli $personeli)
+    {
+        //
+    }
+
+    /**
+     * Handle the personeli "restored" event.
+     *
+     * @param  \App\Personeli  $personeli
+     * @return void
+     */
+    public function restored(Personeli $personeli)
+    {
+        //
+    }
+
+    /**
+     * Handle the personeli "force deleted" event.
+     *
+     * @param  \App\Personeli  $personeli
+     * @return void
+     */
+    public function forceDeleted(Personeli $personeli)
+    {
+        //
+    }
+}
--- app/Personeli.php
+++ app/Personeli.php
@@ -4,11 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\softDeletes;
+use Illuminate\Notifications\Notifiable;
 
 class Personeli extends Model
 {
 
-    use softDeletes;
+    use softDeletes,Notifiable;
 
     protected $dates = ['deleted_at'];
 
